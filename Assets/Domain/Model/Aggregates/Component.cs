bad5dee4aa2893e9c414b685594664549a3a23a6
MatFragg <u202318323@upc.edu.pe>
2025-07-23 20:14:15 -0500

    chore: add Domain events to Class methods

--- Assets/Domain/Model/Aggregates/Component.cs
+++ Assets/Domain/Model/Aggregates/Component.cs
@@ -1,16 +1,20 @@
 using Hampcoders.Electrolink.API.Assets.Domain.ModeL.Commands.Components;
+using Hampcoders.Electrolink.API.Assets.Domain.Model.Events.Components;
 using Hampcoders.Electrolink.API.Assets.Domain.Model.ValueObjects;
+using Hampcoders.Electrolink.API.Shared.Domain.Model.Events;
 
 namespace Hampcoders.Electrolink.API.Assets.Domain.Model.Aggregates;
 
 public class Component
 {
-    public ComponentId Id { get; private set; } 
+    public ComponentId Id { get; private set; }
     public string Name { get; private set; } = string.Empty;
     public string Description { get; private set; } = string.Empty;
-    public bool IsActive { get; private set; } = false;
+    public bool IsActive { get; private set; } = true;
     public ComponentTypeId TypeId { get; private set; } 
-
+    private readonly List<IEvent> _domainEvents = new(); // CAMBIO: Lista de eventos
+    public IReadOnlyList<IEvent> DomainEvents => _domainEvents.AsReadOnly(); 
+    
     public Component() {}
     
     public Component(string name, string description, bool isActive, ComponentTypeId typeId) : this()
@@ -29,6 +33,8 @@ public class Component
         Description = description;
         IsActive = isActive;
         TypeId = typeId;
+        _domainEvents.Add(new ComponentCreatedEvent(Id, TypeId, Name, DateTime.UtcNow));
+
     }
     
     public Component(CreateComponentCommand command) : this(command.Name, command.Description,command.IsActive  , new ComponentTypeId(command.ComponentTypeId))
@@ -42,12 +48,33 @@ public class Component
         Description = command.Description;
         TypeId = new ComponentTypeId(command.TypeId);
         IsActive = command.IsActive;
+
+        _domainEvents.Add(new ComponentUpdatedEvent(
+            Id,
+            Name,
+            Description, 
+            TypeId.Id, 
+            DateTime.UtcNow));
     }
 
     public void Deactivate()
     {
+        if (!IsActive) return;
         IsActive = false;
+        _domainEvents.Add(new ComponentDeactivatedEvent(Id, DateTime.UtcNow)); 
+
     }
     
+    public void Activate()
+    {
+        if (IsActive) return;
+        IsActive = true;
+        _domainEvents.Add(new ComponentActivatedEvent(Id, DateTime.UtcNow)); 
 
+    }
+    
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
 }
