b1acfaab715360f599c7c1613c20a74130a6ec5a
MatFragg <u202318323@upc.edu.pe>
2025-07-24 16:06:59 -0500

    chore: modify property details updated event params

--- Assets/Domain/Model/Events/Properties/PropertyDetailsUpdatedEvent.cs
+++ Assets/Domain/Model/Events/Properties/PropertyDetailsUpdatedEvent.cs
@@ -1,3 +1,8 @@
-﻿namespace Hampcoders.Electrolink.API.Assets.Domain.Model.Events.Properties;
+﻿using Hampcoders.Electrolink.API.Assets.Domain.Model.ValueObjects;
+using Hampcoders.Electrolink.API.Shared.Domain.Model.Events;
 
-public record PropertyDetailsUpdatedEvent();
+namespace Hampcoders.Electrolink.API.Assets.Domain.Model.Events.Properties;
+
+public record PropertyDetailsUpdatedEvent(Guid Id, OwnerId OwnerId, Address Address, Region Region, District District, DateTime Updated): IEvent {
+    public Guid EventId { get; init; } = Guid.NewGuid();
+};
