94ae50b1906d52be6a96f76d459eb03c350fe004
MatFragg <u202318323@upc.edu.pe>
2025-07-26 11:08:07 -0500

    fix: fix IIntegrationEventPublisher declaration

--- Shared/Domain/Services/IIntegrationEventPublisher.cs
+++ Shared/Domain/Services/IIntegrationEventPublisher.cs
@@ -1,6 +1,9 @@
-﻿namespace Hampcoders.Electrolink.API.Shared.Domain.Services;
+﻿using Hampcoders.Electrolink.API.Shared.Domain.Model.Events;
+
+namespace Hampcoders.Electrolink.API.Shared.Domain.Services;
 
 public interface IIntegrationEventPublisher
 {
-    
+    Task PublishAsync<TIntegrationEvent>(TIntegrationEvent integrationEvent, CancellationToken cancellationToken = default)
+        where TIntegrationEvent : IIntegrationEvent;
 }
