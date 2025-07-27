a0063cc47cd324fb785bd222c7f9bcd23e2287c4
MatFragg <u202318323@upc.edu.pe>
2025-07-27 13:49:30 -0500

    chore:implement event handler for property address updated event

--- Assets/Application/Internal/EventHandlers/PropertyCreatedEventHandler.cs
+++ Assets/Application/Internal/EventHandlers/PropertyCreatedEventHandler.cs
@@ -1,6 +1,34 @@
-﻿namespace Hampcoders.Electrolink.API.Assets.Application.Internal.EventHandlers;
+﻿using Hampcoders.Electrolink.API.Assets.Domain.Model.Events.Integration;
+using Hampcoders.Electrolink.API.Assets.Domain.Model.Events.Properties;
+using Hampcoders.Electrolink.API.Shared.Application.Internal.EventHandler;
+using Hampcoders.Electrolink.API.Shared.Domain.Services;
 
-public class PropertyCreatedEventHandler
+namespace Hampcoders.Electrolink.API.Assets.Application.Internal.EventHandlers;
+
+public class PropertyCreatedEventHandler : IEventHandler<PropertyCreatedEvent>
 {
-    
+    private readonly ILogger<PropertyCreatedEventHandler> _logger;
+    private readonly IIntegrationEventPublisher _integrationEventPublisher;
+
+    public PropertyCreatedEventHandler(ILogger<PropertyCreatedEventHandler> logger, IIntegrationEventPublisher integrationEventPublisher)
+    {
+        _logger = logger;
+        _integrationEventPublisher = integrationEventPublisher;
+    }
+
+    public async Task Handle(PropertyCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"[Assets BC] Domain Event: PropertyCreatedEvent recibido para Propiedad: {notification.PropertyId.Id}, Propietario: {notification.OwnerId.Id}.");
+
+        // Publicar evento de integración para un sistema de gestión de clientes (CRM)
+        var integrationEvent = new PropertyCreatedIntegrationEvent(
+            notification.PropertyId,
+            notification.OwnerId,
+            notification.PropertyName,
+            notification.OccurredOn
+        );
+        await _integrationEventPublisher.PublishAsync(integrationEvent, cancellationToken);
+
+        _logger.LogInformation($"[Assets BC] Publicado Integration Event: PropertyCreatedIntegrationEvent para Propiedad: {notification.PropertyId.Id}.");
+    }
 }
