67aed7d5250f1855811afca928cae73782f11c94
MatFragg <u202318323@upc.edu.pe>
2025-07-27 13:49:57 -0500

    chore:implement event handler for property address updated event

--- Assets/Application/Internal/EventHandlers/PropertyAddressUpdatedEventHandler.cs
+++ Assets/Application/Internal/EventHandlers/PropertyAddressUpdatedEventHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace Hampcoders.Electrolink.API.Assets.Application.Internal.EventHandlers;
+﻿using Hampcoders.Electrolink.API.Assets.Domain.Model.Events.Properties;
+using Hampcoders.Electrolink.API.Shared.Application.Internal.EventHandler;
 
-public class PropertyAddressUpdatedEventHandler
+namespace Hampcoders.Electrolink.API.Assets.Application.Internal.EventHandlers;
+
+public class PropertyAddressUpdatedEventHandler : IEventHandler<PropertyAddressUpdatedEvent>
 {
-    
+    private readonly ILogger<PropertyAddressUpdatedEventHandler> _logger;
+
+    public PropertyAddressUpdatedEventHandler(ILogger<PropertyAddressUpdatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(PropertyAddressUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"[Assets BC] Domain Event: Dirección de Propiedad {notification.PropertyId.Id} actualizada a {notification.NewAddress}.");
+        // Actualizar un modelo de lectura geoespacial, por ejemplo.
+        return Task.CompletedTask;
+    }
 }
