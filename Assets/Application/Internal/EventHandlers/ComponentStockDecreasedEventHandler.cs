292499319e56bc3fd8261771c2db15a186ea0f75
MatFragg <u202318323@upc.edu.pe>
2025-07-27 13:50:51 -0500

    chore:implement event handler for component stock decreased event handler

--- Assets/Application/Internal/EventHandlers/ComponentStockDecreasedEventHandler.cs
+++ Assets/Application/Internal/EventHandlers/ComponentStockDecreasedEventHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace Hampcoders.Electrolink.API.Assets.Application.Internal.EventHandlers;
+﻿using Hampcoders.Electrolink.API.Assets.Domain.Model.Events.TechnicianInventories;
+using Hampcoders.Electrolink.API.Shared.Application.Internal.EventHandler;
 
-public class ComponentStockDecreasedEventHandler
+namespace Hampcoders.Electrolink.API.Assets.Application.Internal.EventHandlers;
+
+public class ComponentStockDecreasedEventHandler : IEventHandler<ComponentStockDecreasedEvent>
 {
-    
+    private readonly ILogger<ComponentStockDecreasedEventHandler> _logger;
+
+    public ComponentStockDecreasedEventHandler(ILogger<ComponentStockDecreasedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(ComponentStockDecreasedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"[Assets BC] Domain Event: Stock de Componente {notification.ComponentId.Id} disminuido en {notification.DecreasedQuantity}. Nuevo total: {notification.NewQuantityAvailable}.");
+        // Aquí podrías actualizar un modelo de lectura, o realizar alguna otra acción interna.
+        return Task.CompletedTask;
+    }
 }
