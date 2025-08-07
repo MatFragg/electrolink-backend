cb3f5d89b83a8781cc9d9e06eba5a680b9dfc7ee
MatFragg <u202318323@upc.edu.pe>
2025-08-06 23:29:09 -0500

    chore: fix property detail updated event

--- Assets/Domain/Model/Events/Properties/PropertyDetailsUpdatedEvent.cs
+++ Assets/Domain/Model/Events/Properties/PropertyDetailsUpdatedEvent.cs
@@ -3,6 +3,6 @@ using Hampcoders.Electrolink.API.Shared.Domain.Model.Events;
 
 namespace Hampcoders.Electrolink.API.Assets.Domain.Model.Events.Properties;
 
-public record PropertyDetailsUpdatedEvent(Guid Id, OwnerId OwnerId, Address Address, Region Region, District District, DateTime Updated): IEvent {
+public record PropertyDetailsUpdatedEvent(Guid Id, OwnerId OwnerId, Address Address, Region Region, District District, DateTime OccurredOn): IEvent {
     public Guid EventId { get; init; } = Guid.NewGuid();
 };
