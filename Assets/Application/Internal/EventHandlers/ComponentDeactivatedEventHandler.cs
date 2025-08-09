7c0ee02c90d1287389f648e848414bd043417e44
MatFragg <u202318323@upc.edu.pe>
2025-08-09 12:22:34 -0500

    ﻿feat(assets-and-resources-management:application): implement ComponentDeactivatedEventHandler class

--- Assets/Application/Internal/EventHandlers/ComponentDeactivatedEventHandler.cs
+++ Assets/Application/Internal/EventHandlers/ComponentDeactivatedEventHandler.cs
@@ -1,6 +1,31 @@
-﻿namespace Hampcoders.Electrolink.API.Assets.Application.Internal.EventHandlers;
+﻿using Hampcoders.Electrolink.API.Assets.Domain.ModeL.Events.Components;
+using Hampcoders.Electrolink.API.Assets.Domain.Model.Events.Integration;
+using Hampcoders.Electrolink.API.Shared.Application.Internal.EventHandler;
+using Hampcoders.Electrolink.API.Shared.Domain.Services;
 
-public class ComponentDeactivatedEventHandler
+namespace Hampcoders.Electrolink.API.Assets.Application.Internal.EventHandlers;
+
+public class ComponentDeactivatedEventHandler : IEventHandler<ComponentDeactivatedEvent>
 {
-    
+    private readonly ILogger<ComponentDeactivatedEventHandler> _logger;
+    private readonly IIntegrationEventPublisher _integrationEventPublisher; 
+
+    public ComponentDeactivatedEventHandler(ILogger<ComponentDeactivatedEventHandler> logger, IIntegrationEventPublisher integrationEventPublisher)
+    {
+        _logger = logger;
+        _integrationEventPublisher = integrationEventPublisher;
+    }
+
+    public async Task Handle(ComponentDeactivatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"[Assets BC] Domain Event: ComponentDeactivatedEvent recibido para Componente: {notification.ComponentId.Id}.");
+        var integrationEvent = new ComponentStateChangedIntegrationEvent(
+            notification.ComponentId,
+            false,
+            notification.OccurredOn
+        );
+        await _integrationEventPublisher.PublishAsync(integrationEvent, cancellationToken);
+        _logger.LogInformation($"[Assets BC] Publicado Integration Event: ComponentStateChangedIntegrationEvent para Componente: {notification.ComponentId.Id}.");
+        return;
+    }
 }
