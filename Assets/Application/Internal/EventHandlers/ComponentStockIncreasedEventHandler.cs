8a2333700630ce021e0a81c07fef25a0faea3e31
MatFragg <u202318323@upc.edu.pe>
2025-08-09 12:14:30 -0500

    ﻿feat(assets-and-resources-management:application): implement ComponentStockIncreasedEventHandler class

--- Assets/Application/Internal/EventHandlers/ComponentStockIncreasedEventHandler.cs
+++ Assets/Application/Internal/EventHandlers/ComponentStockIncreasedEventHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace Hampcoders.Electrolink.API.Assets.Application.Internal.EventHandlers;
+﻿using Hampcoders.Electrolink.API.Assets.Domain.Model.Events.TechnicianInventories;
+using Hampcoders.Electrolink.API.Shared.Application.Internal.EventHandler;
 
-public class ComponentStockIncreasedEventHandler
+namespace Hampcoders.Electrolink.API.Assets.Application.Internal.EventHandlers;
+
+public class ComponentStockIncreasedEventHandler : IEventHandler<ComponentStockIncreasedEvent>
 {
-    
+    private readonly ILogger<ComponentStockIncreasedEventHandler> _logger;
+
+    public ComponentStockIncreasedEventHandler(ILogger<ComponentStockIncreasedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(ComponentStockIncreasedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"[Assets BC] Domain Event: Stock de Componente {notification.ComponentId.Id} aumentado en {notification.IncreasedQuantity}. Nuevo total: {notification.NewQuantityAvailable}.");
+        // No necesariamente requiere un evento de integración a menos que haya un sistema de compras/logística externo.
+        return Task.CompletedTask;
+    }
 }
