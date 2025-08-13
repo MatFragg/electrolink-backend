219b75adbf537d09cf7b42d763b0f9a211b8fe25
MatFragg <u202318323@upc.edu.pe>
2025-08-12 22:44:22 -0500

    refactor(identity-and-access-management:domain): implement UpdatePasswordCommand record

--- IAM/Domain/Model/Commands/UpdatePasswordCommand.cs
+++ IAM/Domain/Model/Commands/UpdatePasswordCommand.cs
@@ -1,3 +1,3 @@
 ﻿namespace Hampcoders.Electrolink.API.IAM.Domain.Model.Commands;
 
-public record UpdatePasswordCommand(int UserId, string NewPassword);
+public record UpdatePasswordCommand(int UserId,string CurrentPassword, string NewPassword);
