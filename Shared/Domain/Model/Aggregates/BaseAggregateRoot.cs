53151ca156647093d4359c67069783ca1ebcc6e0
MatFragg <u202318323@upc.edu.pe>
2026-02-19 10:40:51 -0500

    refact: add Base Aggregate Root model class and define some common exceptions

--- Shared/Domain/Model/Aggregates/BaseAggregateRoot.cs
+++ Shared/Domain/Model/Aggregates/BaseAggregateRoot.cs
@@ -1,6 +1,25 @@
-﻿namespace Hampcoders.Electrolink.API.Shared.Domain.Model.Aggregates;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Hampcoders.Electrolink.API.Shared.Domain.Model.Events;
 
-public class BaseAggregateRoot
+namespace Hampcoders.Electrolink.API.Shared.Domain.Model.Aggregates;
+
+public abstract class BaseAggregateRoot
 {
-    
+    private readonly List<IEvent> _domainEvents = [];
+
+    public IReadOnlyCollection<IEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void RaiseDomainEvent(IEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+
 }
--- Shared/Domain/Model/Exceptions/InvalidIdException.cs
+++ Shared/Domain/Model/Exceptions/InvalidIdException.cs
@@ -1,6 +1,34 @@
-﻿namespace Hampcoders.Electrolink.API.Shared.Domain.Model.Exceptions;
+﻿using System.Runtime.Serialization;
+namespace Hampcoders.Electrolink.API.Shared.Domain.Model.Exceptions;
 
-public class InvalidIdException
+[Serializable]
+public class InvalidIdException : Exception
 {
-    
+    public string IdType { get; }
+    public string InvalidValue { get; }
+
+    public InvalidIdException(string idType, string invalidValue)
+        : base($"Invalid {idType} id: '{invalidValue}'")
+    {
+        IdType = idType;
+        InvalidValue = invalidValue;
+    }
+
+    public InvalidIdException(string message) : base(message) { }
+
+    public InvalidIdException(string message, Exception inner) : base(message, inner) { }
+
+    protected InvalidIdException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+        IdType = info.GetString(nameof(IdType));
+        InvalidValue = info.GetString(nameof(InvalidValue));
+    }
+
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        if (info == null) throw new ArgumentNullException(nameof(info));
+        info.AddValue(nameof(IdType), IdType);
+        info.AddValue(nameof(InvalidValue), InvalidValue);
+        base.GetObjectData(info, context);
+    }
 }
