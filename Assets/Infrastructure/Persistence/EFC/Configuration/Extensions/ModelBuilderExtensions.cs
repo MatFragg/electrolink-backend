331a3ec1ad8451459c6bcfaa43f57816b36dff0f
MatFragg <u202318323@upc.edu.pe>
2026-03-02 11:19:58 -0500

    refact(assets-and-resources-management:application): update infrastructure layer according to needed modifications.

--- Assets/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
+++ Assets/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -1,6 +1,7 @@
 using Hampcoders.Electrolink.API.Assets.Domain.Model.Aggregates;
 using Hampcoders.Electrolink.API.Assets.Domain.Model.Entities;
 using Hampcoders.Electrolink.API.Assets.Domain.Model.ValueObjects;
+using Hampcoders.Electrolink.API.Shared.Domain.Model.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace Hampcoders.Electrolink.API.Assets.Infrastructure.Persistence.EFC.Configuration.Extensions;
@@ -9,106 +10,254 @@ public static class ModelBuilderExtensions
 {
     public static void ApplyAssetsConfiguration(this ModelBuilder builder)
     {
-        // --- Configuración para ComponentType ---
-        builder.Entity<ComponentType>(b =>
+        /*
+         * Technician Inventory
+         */
+        builder.Entity<TechnicianInventory>().HasKey(ti => ti.Id);
+        builder.Entity<TechnicianInventory>()
+            .Property(ti => ti.Id)
+            .HasConversion(id => id.Value, value => TechnicianInventoryId.From(value))
+            .HasColumnName("Id")
+            .IsRequired()
+            .ValueGeneratedNever();
+
+        builder.Entity<TechnicianInventory>()
+            .Property(ti => ti.TechnicianId)
+            .HasConversion(id => id.Value, value => TechnicianId.From(value))
+            .HasColumnName("TechnicianId")
+            .IsRequired();
+
+        builder.Entity<TechnicianInventory>()
+            .Property(ti => ti.Status)
+            .HasConversion<string>()
+            .HasColumnName("Status")
+            .IsRequired();
+
+        builder.Entity<TechnicianInventory>()
+            .HasMany(ti => ti.StockItems)
+            .WithOne()
+            .HasForeignKey("TechnicianInventoryId")
+            .IsRequired();
+
+        builder.Entity<TechnicianInventory>()
+            .HasMany(ti => ti.Reservations)
+            .WithOne()
+            .HasForeignKey("TechnicianInventoryId")
+            .IsRequired();
+
+        /*
+         * Component Stock
+         */
+        builder.Entity<ComponentStock>().HasKey(cs => cs.Id);
+        builder.Entity<ComponentStock>()
+            .Property(cs => cs.Id)
+            .HasConversion(id => id.Value, value => ComponentStockId.From(value))
+            .HasColumnName("Id")
+            .IsRequired()
+            .ValueGeneratedNever();
+
+        builder.Entity<ComponentStock>()
+            .Property(cs => cs.TechnicianInventoryId)
+            .HasConversion(id => id.Value, value => TechnicianInventoryId.From(value))
+            .HasColumnName("TechnicianInventoryId")
+            .IsRequired();
+
+        builder.Entity<ComponentStock>()
+            .Property(cs => cs.ComponentId)
+            .HasConversion(id => id.Value, value => ComponentId.From(value))
+            .HasColumnName("ComponentId")
+            .IsRequired();
+
+        /*
+         * Component Reservation
+         */
+        builder.Entity<ComponentReservation>().HasKey(cr => cr.Id);
+        builder.Entity<ComponentReservation>()
+            .Property(cr => cr.Id)
+            .HasConversion(id => id.Value, value => ComponentReservationId.From(value))
+            .HasColumnName("Id")
+            .IsRequired()
+            .ValueGeneratedNever();
+
+        builder.Entity<ComponentReservation>()
+            .Property(cr => cr.TechnicianInventoryId)
+            .HasConversion(id => id.Value, value => TechnicianInventoryId.From(value))
+            .HasColumnName("TechnicianInventoryId")
+            .IsRequired();
+
+        builder.Entity<ComponentReservation>()
+            .Property(cr => cr.ServiceId)
+            .HasConversion(id => id.Value, value => ServiceId.From(value))
+            .HasColumnName("ServiceId")
+            .IsRequired();
+
+        builder.Entity<ComponentReservation>()
+            .HasMany(cr => cr.Items)
+            .WithOne()
+            .HasForeignKey("ReservationId")
+            .IsRequired();
+
+        /*
+         * Reservation Item
+         */
+        builder.Entity<ReservationItem>().HasKey(ri => ri.Id);
+        builder.Entity<ReservationItem>()
+            .Property(ri => ri.Id)
+            .HasConversion(id => id.Value, value => ReservationItemId.From(value))
+            .HasColumnName("Id")
+            .IsRequired()
+            .ValueGeneratedNever();
+
+        builder.Entity<ReservationItem>()
+            .Property(ri => ri.ReservationId)
+            .HasConversion(id => id.Value, value => ComponentReservationId.From(value))
+            .HasColumnName("ReservationId")
+            .IsRequired();
+
+        builder.Entity<ReservationItem>()
+            .Property(ri => ri.ComponentId)
+            .HasConversion(id => id.Value, value => ComponentId.From(value))
+            .HasColumnName("ComponentId")
+            .IsRequired();
+
+        /*
+         * Component Type
+         */
+        builder.Entity<ComponentType>().HasKey(ct => ct.Id);
+        builder.Entity<ComponentType>()
+            .Property(ct => ct.Id)
+            .HasConversion(id => id.Value, value => ComponentTypeId.From(value))
+            .HasColumnName("Id")
+            .IsRequired()
+            .ValueGeneratedNever();
+
+        builder.Entity<ComponentType>().HasIndex(ct => ct.Name).IsUnique();
+
+        /*
+         * Component
+         */
+        builder.Entity<Component>().HasKey(c => c.Id);
+        builder.Entity<Component>()
+            .Property(c => c.Id)
+            .HasConversion(id => id.Value, value => ComponentId.From(value))
+            .HasColumnName("Id")
+            .IsRequired()
+            .ValueGeneratedNever();
+
+        builder.Entity<Component>()
+            .Property(c => c.TypeId)
+            .HasConversion(id => id.Value, value => ComponentTypeId.From(value))
+            .HasColumnName("TypeId")
+            .IsRequired();
+
+        builder.Entity<Component>().HasIndex(c => c.Name);
+        builder.Entity<Component>().HasIndex(c => c.IsActive);
+        builder.Entity<Component>().HasIndex(c => c.TypeId);
+
+        /*
+         * Property Portfolio
+         */
+        builder.Entity<PropertyPortfolio>().HasKey(pp => pp.Id);
+        builder.Entity<PropertyPortfolio>()
+            .Property(pp => pp.Id)
+            .HasConversion(id => id.Value, value => PropertyPortfolioId.From(value))
+            .HasColumnName("Id")
+            .IsRequired()
+            .ValueGeneratedNever();
+
+        builder.Entity<PropertyPortfolio>()
+            .Property(pp => pp.HomeownerId)
+            .HasConversion(id => id.Value, value => HomeownerId.From(value))
+            .HasColumnName("HomeownerId")
+            .IsRequired();
+
+        builder.Entity<PropertyPortfolio>()
+            .Property(pp => pp.Status)
+            .HasConversion<string>()
+            .HasColumnName("Status")
+            .IsRequired();
+
+        builder.Entity<PropertyPortfolio>()
+            .HasMany(pp => pp.Entries)
+            .WithOne()
+            .HasForeignKey("PortfolioId")
+            .IsRequired();
+
+        /*
+         * Portfolio Entry
+         */
+        builder.Entity<PortfolioEntry>().HasKey(pe => pe.Id);
+        builder.Entity<PortfolioEntry>()
+            .Property(pe => pe.Id)
+            .HasConversion(id => id.Value, value => PortfolioEntryId.From(value))
+            .HasColumnName("Id")
+            .IsRequired()
+            .ValueGeneratedNever();
+
+        builder.Entity<PortfolioEntry>()
+            .Property(pe => pe.PortfolioId)
+            .HasConversion(id => id.Value, value => PropertyPortfolioId.From(value))
+            .HasColumnName("PortfolioId")
+            .IsRequired();
+
+        builder.Entity<PortfolioEntry>()
+            .Property(pe => pe.PropertyId)
+            .HasConversion(id => id.Value, value => PropertyId.From(value))
+            .HasColumnName("PropertyId")
+            .IsRequired();
+
+        builder.Entity<PortfolioEntry>()
+            .Property(pe => pe.OccupancyStatus)
+            .HasConversion<string>()
+            .IsRequired();
+
+        /*
+         * Property
+         */
+        builder.Entity<Property>().HasKey(prop => prop.Id);
+        builder.Entity<Property>()
+            .Property(p => p.Id)
+            .HasConversion(id => id.Value, value => PropertyId.From(value))
+            .HasColumnName("Id")
+            .IsRequired()
+            .ValueGeneratedNever();
+
+        builder.Entity<Property>()
+            .Property(p => p.OwnerId)
+            .HasConversion(id => id.Value, value => HomeownerId.From(value))
+            .HasColumnName("OwnerId")
+            .IsRequired();
+
+        builder.Entity<Property>().HasIndex(p => p.OwnerId);
+
+        builder.Entity<Property>().OwnsOne(p => p.Address, addr =>
         {
-            b.ToTable("component_types");
-            b.HasKey(ct => ct.Id);
-            b.Property(ct => ct.Id).IsRequired().ValueGeneratedOnAdd();
-            b.Property(ct => ct.Name).IsRequired().HasMaxLength(50);
-            b.Property(ct => ct.Description).HasMaxLength(250);
-            b.Property(ct => ct.Id).HasConversion(id => id.Id, value => new ComponentTypeId(value));
-        });
-
-        // --- Configuración para Component ---
-        builder.Entity<Component>(b =>
-        {
-            b.ToTable("components");
-            b.HasKey(c => c.Id);
-            b.Property(c => c.Id).HasConversion(id => id.Id, value => new ComponentId(value));
-            b.Property(c => c.Name).IsRequired().HasMaxLength(100);
-            b.Property(c => c.Description).HasMaxLength(500);
-            b.Property(c => c.IsActive).IsRequired();
-            b.Property(c => c.TypeId).HasConversion(id => id.Id, value => new ComponentTypeId(value)).HasColumnName("component_type_id");
-            b.HasOne<ComponentType>().WithMany().HasForeignKey(c => c.TypeId).IsRequired();
-        });
-
-        // --- Configuración para Property ---
-        builder.Entity<Property>(prop =>
-        {
-            prop.ToTable("properties");
-
-            // PK
-            prop.HasKey(p => p.Id);
-            prop.Property(p => p.Id)
-                .HasConversion(new StronglyTypedIdConverter<PropertyId>())
-                .HasColumnName("property_id")
-                .IsRequired()
-                .ValueGeneratedNever(); // lo generas tú en el constructor
-
-            // Foreign owner
-            prop.Property(p => p.OwnerId)
-                .HasConversion(new StronglyTypedIdConverter<OwnerId>())
-                .HasColumnName("owner_id")
-                .IsRequired();
-
-            // ---- Owned: Address -------------------------------
-            prop.OwnsOne(p => p.Address, addr =>
-            {
-                addr.Property(a => a.Street).IsRequired().HasMaxLength(120).HasColumnName("street");
-                addr.Property(a => a.Number).IsRequired().HasMaxLength(20).HasColumnName("number");
-            });
-
-            // ---- Owned: Region -------------------------------
-            prop.OwnsOne(p => p.Region, reg =>
-            {
-                reg.Property(r => r.Name).IsRequired().HasMaxLength(60).HasColumnName("region");
-            });
-
-            // ---- Owned: District ------------------------------
-            prop.OwnsOne(p => p.District, dist =>
-            {
-                dist.Property(d => d.Name).IsRequired().HasMaxLength(60).HasColumnName("district");
-            });
-        });
-        builder.Entity<TechnicianInventory>(b =>
-        {
-            b.ToTable("technician_inventories");
-            b.HasKey(i => i.Id);
-            b.Property(i => i.Id).IsRequired().ValueGeneratedNever();
+            addr.WithOwner().HasForeignKey("Id");
     
-            b.Property(i => i.TechnicianId)
-                .HasConversion(id => id.Id, value => new TechnicianId(value))
-                .IsRequired();
-            b.HasIndex(i => i.TechnicianId).IsUnique();
-    
-            b.HasMany(i => i.StockItems)
-                .WithOne(s => s.TechnicianInventory)
-                .HasForeignKey(cs => cs.TechnicianInventoryId)
-                .IsRequired();
+            addr.Property(s => s.Street).HasColumnName("Street");
+            addr.Property(s => s.District).HasColumnName("District");
+            addr.Property(s => s.City).HasColumnName("City");
+            addr.Property(s => s.PostalCode).HasColumnName("PostalCode");
+            addr.Property(s => s.Country).HasColumnName("Country");
         });
 
-        builder.Entity<ComponentStock>(b =>
+        builder.Entity<Property>().OwnsOne(p => p.Geolocation, geo =>
         {
-            b.ToTable("component_stocks");
-            b.HasKey(cs => cs.Id);
-            b.Property(cs => cs.Id).IsRequired().ValueGeneratedNever();
-            b.Property(cs => cs.ComponentId)
-                .HasConversion(id => id.Id, value => new ComponentId(value))
-                .HasColumnName("component_id")
-                .IsRequired();
-
-            b.HasIndex(cs => new { cs.TechnicianInventoryId, cs.ComponentId }).IsUnique();
+            geo.WithOwner().HasForeignKey("Id");
     
-            b.HasOne<Component>()
-                .WithMany()
-                .HasForeignKey(cs => cs.ComponentId)
-                .IsRequired();
+            geo.Property(g => g.Latitude).HasColumnName("Latitude");
+            geo.Property(g => g.Longitude).HasColumnName("Longitude");
+            geo.Property(g => g.Accuracy).HasColumnName("Accuracy");
+            geo.Property(g => g.Source).HasColumnName("Source");
+        });
 
-            b.Property(cs => cs.TechnicianInventoryId).IsRequired();
-            b.Property(cs => cs.QuantityAvailable).IsRequired();
-            b.Property(cs => cs.AlertThreshold).IsRequired();
-            b.Property(cs => cs.LastUpdated).IsRequired();
-        }); 
+        builder.Entity<Property>()
+            .Property(p => p.Status)
+            .HasConversion<string>()
+            .IsRequired();
+
+        builder.Entity<Property>()
+            .Property(p => p.IsActive)
+            .IsRequired();
     }
 }
--- Assets/Infrastructure/Persistence/EFC/Repositories/PropertyRepository.cs
+++ Assets/Infrastructure/Persistence/EFC/Repositories/PropertyRepository.cs
@@ -15,12 +15,7 @@ public class PropertyRepository(AppDbContext context) : BaseRepository<Property,
             .Where(p => p.OwnerId == homeownerId)
             .ToListAsync();
     }
-
-    public Task<Property?> FindByIdAndOwnerIdAsync(Domain.Model.ValueObjects.PropertyId propertyId, HomeownerId homeownerId)
-    {
-        throw new NotImplementedException();
-    }
-
+    
     public async Task<IEnumerable<Property>> FindByCityAsync(string city)
     {
         // Implementar la lógica para buscar propiedades por ciudad
@@ -38,42 +33,13 @@ public class PropertyRepository(AppDbContext context) : BaseRepository<Property,
             .ToListAsync();
     }
 
-    public async Task<IEnumerable<Property>> FindByRegionAsync(string regionName)
-    {
-        // Implementar la lógica para buscar propiedades por región
-        return await Context.Properties
-            .Where(p => p.Region.Name == regionName)
-            .ToListAsync();
-    }
-
-    public async Task<IEnumerable<Property>> FindByDistrictAsync(string districtName)
-    {
-        // Implementar la lógica para buscar propiedades por distrito
-        return await Context.Properties
-            .Where(p => p.District.Name == districtName)
-            .ToListAsync();
-    }
-    
-    /*public async Task<Property?> Handle(DeactivatePropertyCommand command)
-    {
-        var property = await FindByIdAsync(new PropertyId(command.Id));
-        if (property == null) return null;
-        
-        property.Handle(command);
-        await Context.SaveChangesAsync();
-        return property;
-    }*/
-    
     public async Task<Property?> FindByIdAndOwnerIdAsync(PropertyId propertyId, HomeownerId ownerId)
     {
         return await Context.Set<Property>()
             .FirstOrDefaultAsync(p => p.Id == propertyId && p.OwnerId == ownerId);
     }
-    public async Task<IEnumerable<Property>> GetAllFilteredAsync(
-        HomeownerId ownerId, 
-        string? city, 
-        string? district, 
-        string? region, 
+    public async Task<IEnumerable<Property>> GetAllFilteredAsync(HomeownerId ownerId,
+        string? city,
         string? street)
     {
         var query = Context.Set<Property>().Where(p => p.OwnerId == ownerId);
@@ -81,14 +47,6 @@ public class PropertyRepository(AppDbContext context) : BaseRepository<Property,
         if (!string.IsNullOrWhiteSpace(city))
         {
             query = query.Where(p => p.Address.City.Contains(city));
-        }
-        if (!string.IsNullOrWhiteSpace(district))
-        {
-            query = query.Where(p => p.District.Name == district);
-        }
-        if (!string.IsNullOrWhiteSpace(region))
-        {
-            query = query.Where(p => p.Region.Name == region);
         }
         if (!string.IsNullOrWhiteSpace(street))
         {
